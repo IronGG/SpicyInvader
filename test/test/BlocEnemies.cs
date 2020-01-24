b537557836dc3c1324d3289a2fbbe892776cf38f
IronGG <60259644+IronGG@users.noreply.github.com>
2020-01-24 13:46:15 +0100

    Add files via upload
    First test version

---
+++ test/test/BlocEnemies.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class BlocEnemies
+    {
+        bool right;
+        bool left;
+
+        int posX;
+        int posY;
+
+        int[,] blocEnemy = new int[8,8];
+
+        public BlocEnemies()
+        {
+            right = true;
+            left = false;
+            const int spawnX = 0;
+            const int spawnY = 0;
+
+            posX = 0;
+            posY = 0;
+        }
+
+        public void CreateGrid()
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    blocEnemy[x, y] = 0;
+                }
+            }
+        }
+
+        public void RespawnAllEnemies()
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    blocEnemy[x, y] = 1;
+                }
+            }
+        }
+
+        public void DisplayEnemies()
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    Console.SetCursorPosition(posX + y, posY + x);
+                    switch (blocEnemy[x, y])
+                    {
+                        case 0:
+                            Console.Write(" ");
+                            break;
+
+                        case 1 :
+                            Console.Write("☺");
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void EnemiesMovement()
+        {
+            if (right)
+            {
+                right = true;
+                posX++;
+
+                for (int x = 0; x < 8; x++)
+                {
+                    for (int y = 0; y < 8; y++)
+                    {
+                        if (posX + 8 == Console.WindowWidth && right)
+                        {
+                            left = true;
+                            right = false;
+                            posX--;
+                            Console.SetCursorPosition(posX + y, posY + x);
+                            Console.Write(" ");
+
+                            if (posY + x == 0)
+                            {
+                                Console.SetCursorPosition(posX + y, posY - 1);
+                                Console.Write(" ");
+                            }
+
+                            posY++;
+                        }
+
+                        if (y == 0)
+                        {
+                            Console.SetCursorPosition(posX + y - 1, posY + x);
+                            Console.Write(" ");
+                        }
+
+
+
+                        Console.SetCursorPosition(posX + y, posY + x);
+                        switch (blocEnemy[x, y])
+                        {
+                            case 0:
+                                Console.Write(" ");
+                                break;
+
+                            case 1:
+                                Console.Write("☺");
+                                break;
+                        }
+                    }
+                }
+            }
+
+            else if (left)
+            {
+                posX--;
+
+                for (int x = 0; x < 8; x++)
+                {
+                    for (int y = 0; y < 8; y++)
+                    {
+                        if (posX + y == 0)
+                        {
+                            left = false;
+                            right = true;
+                            posX++;
+                            Console.SetCursorPosition(posX + y - 1, posY + x);
+                            Console.Write(" ");
+                            Console.SetCursorPosition(posX + y - 1, posY + x - 1);
+                            Console.Write(" ");
+                            posY++;
+                        }
+
+                        if (y == 7)
+                        {
+                            Console.SetCursorPosition(posX + y + 1, posY + x);
+                            Console.Write(" ");
+                        }
+
+
+
+                        Console.SetCursorPosition(posX + y, posY + x);
+                        switch (blocEnemy[x, y])
+                        {
+                            case 0:
+                                Console.Write(" ");
+                                break;
+
+                            case 1:
+                                Console.Write("☺");
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/test/Program.cs
@@ -0,0 +1,159 @@
+﻿//Auteur : JMY
+//Date   : 03.09.2018
+//Lieu   : ETML
+//Description : Squelette pour SpaceInvaders en console
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test;
+
+namespace spaceInvadersSquelette
+{
+    class Program
+    {
+        //Graphisme avancé du vaisseau
+        const string ship = "^";
+        const int shipY = 21;//position verticale du vaisseau
+
+        static void Main(string[] args)
+        {
+            BlocEnemies BlocEnemies = new BlocEnemies();
+            Random rndPosEnemy = new Random();
+
+            int shipX = 50;
+
+            int score = 0;
+
+            int missileX = -1;
+            int missileY = -1;
+
+            int enemyX = 5;
+            int enemyY = 1;
+
+            bool missileFired = false;
+
+            int tics = 0;
+
+
+            Console.SetCursorPosition(enemyX, enemyY);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("☺");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            BlocEnemies.RespawnAllEnemies();
+            BlocEnemies.DisplayEnemies();
+
+            while (true)
+            {
+                //Utilisateur a appuyé sur une touche ?
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    switch (key.Key)
+                    {
+                        //Touche fléchée gauche
+                        case ConsoleKey.LeftArrow:
+                            //Décalage de la position de référence
+                            Console.SetCursorPosition(shipX, shipY);
+                            Console.Write(" ");//Effacer l'ancienne position
+                            shipX--;
+
+                            break;
+
+                        case ConsoleKey.RightArrow:
+
+                             //Décalage de la position de référence
+                             Console.SetCursorPosition(shipX, shipY);
+                             Console.Write(" ");//Effacer l'ancienne position
+                             shipX++;
+                            break;
+
+                        //MISSILE
+                        case ConsoleKey.Spacebar:
+
+                            //Un seul missile à la fois
+                            if (!missileFired)
+                            {
+                                missileFired = true;
+                                missileY = shipY;
+                                missileX = shipX;//Placement du missile initial
+                            }
+                            break;
+                    }
+
+                }
+
+                //On affiche le vaisseau à la bonne positition
+                Console.SetCursorPosition(shipX, shipY);
+                Console.Write(ship);
+
+                if (tics % 40 == 0)
+                {
+                    //Effacer l'ancien missile
+                    /*Console.SetCursorPosition(enemyX, enemyY);
+                    Console.Write(" ");
+                    enemyY++;
+                    //AFficher la nouvelle position
+                    Console.SetCursorPosition(enemyX, enemyY);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("☺");
+                    Console.ForegroundColor = ConsoleColor.Gray;*/
+
+                    BlocEnemies.EnemiesMovement(); //TEST ATTENTION !
+                }
+
+                //Gestion du missile
+                if (missileFired)
+                {
+
+                    if(enemyX == missileX && enemyY == missileY)
+                    {
+                        Console.SetCursorPosition(enemyX, enemyY);
+                        Console.Write(" ");
+                        score++;
+                        Console.SetCursorPosition(0, 0);
+                        Console.Write($"Le score est de {score}");
+
+                        enemyX = rndPosEnemy.Next(Console.WindowWidth);
+                        enemyY = 0;
+                    }
+
+                    if (missileY > 0)
+                    {
+                        //Réduire la vitesse du missile
+                        if (tics % 5 == 0)
+                        {
+                            //Effacer l'ancien missile
+                            Console.SetCursorPosition(missileX, missileY);
+                            Console.Write(" ");
+
+                            //AFficher la nouvelle position
+                            Console.SetCursorPosition(missileX, --missileY);
+                            Console.Write("*");
+                        }
+                    }
+                    else
+                    {
+                        //Fin du missile
+                        Console.SetCursorPosition(missileX, missileY);
+                        Console.Write(" ");
+                        missileFired = false;
+                    }
+                }
+
+                //Temporisation FPS
+                System.Threading.Thread.Sleep(2);
+
+                tics++;
+                if (tics == Int32.MaxValue)
+                    tics = 0;
+
+            }
+
+        }
+    }
+}
